e23e85cf94d53a82c350508fef918345ec67fd6a
huyhoanFithcmus <huyhoan652002@gmail.com>
2023-01-26 11:33:24 +0700

    add API and done project

--- ChatGPTMobile/App.js
+++ ChatGPTMobile/App.js
@@ -1,5 +1,11 @@
-import React, { useState, useCallback, useEffect } from 'react'
-import { GiftedChat } from 'react-native-gifted-chat'
+import React, {useState, useCallback, useEffect} from 'react';
+import {SafeAreaView} from 'react-native';
+import {GiftedChat} from 'react-native-gifted-chat';
+
+// const logo = require('./assets/logo.png');
+const API_URL = 'https://api.openai.com/v1/completions';
+const YOUR_API_KEY = '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';
+const MAX_TOKENS = 100;
 
 export default function Example() {
   const [messages, setMessages] = useState([]);
@@ -13,23 +19,69 @@ export default function Example() {
         user: {
           _id: 2,
           name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
+          // avatar: logo,
         },
       },
-    ])
-  }, [])
+    ]);
+  }, []);
 
   const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-  }, [])
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    );
+    const value = messages[0].text;
+    callApi(value);
+  }, []);
+
+  const callApi = async value => {
+    const res = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          `Bearer ${YOUR_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: YOUR_API_KEY,
+        prompt: value,
+        max_tokens: MAX_TOKENS,
+        temperature: 0,
+      }),
+    });
+
+    const data = await res.json();
+
+    if (data) {
+      const value = data?.choices[0]?.text;
+      addNewMessage(value);
+      console.log('Data: ', value);
+    }
+  };
+
+  const addNewMessage = data => {
+    const value = {
+      _id: Math.random(999999999999),
+      text: data,
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'React Native',
+        avatar: logo,
+      },
+    };
+
+    setMessages(previousMessages => GiftedChat.append(previousMessages, value));
+  };
 
   return (
-    <GiftedChat
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1,
-      }}
-    />
-  )
+    <SafeAreaView style={{flex: 1}}>
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    </SafeAreaView>
+  );
 }
